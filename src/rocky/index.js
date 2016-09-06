1fc435c4cb10faad593044b3011a44d3cb2053b5
Dries Oeyen <dries@nexworx.com>
2016-09-07 01:21:15 +0200

    Display current time

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,9 +1,20 @@
 var rocky = require('rocky');
 
+rocky.on('secondchange', function(e) {
+	// When the wall time second changes, redraw the screen
+	// TODO: Watch what happens if you change this to 'minutechange'!
+	rocky.requestDraw();
+});
+
 rocky.on('draw', function(e) {
 	// Get the screen as a CanvasRenderingContext2D
 	var ctx = e.context;
 	
-	// Show some text in the canvas
-	ctx.fillText("Hello Belgium!", 10, 10);
+	// Reset the canvas
+	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+	ctx.fillStyle = 'green'; // Enable Matrix-mode
+	
+	// Show the time in the canvas
+	var time = new Date().toLocaleTimeString();
+	ctx.fillText(time, 10, 10);
 });
